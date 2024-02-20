d43e0c31cb7669153b1342f7b184e76a699792f9
Hamza Tahir <90556877+Hamza-Tahirr@users.noreply.github.com>
2024-02-20 14:12:51 +0000

    init

--- chatpdf/src/app/page.tsx
+++ chatpdf/src/app/page.tsx
@@ -4,7 +4,11 @@ import { Button } from "@/components/ui/button";
 export default async function Home() {
   return (
    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="">
 
+      </div>
+    </div>
    </div>
   );
 }
