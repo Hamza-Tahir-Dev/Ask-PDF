6a12615572fa526b966d3522b56a92930cd9eb41
Hamza Tahir <90556877+Hamza-Tahirr@users.noreply.github.com>
2024-02-21 09:52:27 +0000

    init fe

--- chatpdf/src/components/FileUpload.tsx
+++ chatpdf/src/components/FileUpload.tsx
@@ -1,34 +1,90 @@
-'use client'
-import { Inbox } from "lucide-react";
-import React from "react";
-import { useDropzone } from "react-dropzone";
+import React,{useState} from 'react'
+// Import the main component
+import { Viewer } from '@react-pdf-viewer/core'; // install this library
+// Plugins
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'; // install this library
+// Import the styles
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+// Worker
+import { Worker } from '@react-pdf-viewer/core'; // install this library
 
-type Props = {}
+export const App = () => {
 
-const FileUpload = (props : Props) => {
-    const {getRootProps, getInputProps} = useDropzone({
-        accept: { "application/pdf": [".pdf"] },
-        maxFiles: 1,
-        onDrop: async (acceptedFiles) => {
-        console.log(acceptedFiles);
-        const file = acceptedFiles[0]
-        },
-    });
-          
-    return (
-        <div className="p-2 bg-white rounded-xl">
-            <div {...getRootProps(
-                {
-                    className: 'border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-5 flex justify-center items-center flex-col', 
-                }
-            )}>
-                <input {...getInputProps()}/>
-                <>
-                <Inbox className="w-10 h-10 text-blue-500"/>
-                <p className="mt-2 text-sm text-slate-400">Drop Pdf Here</p>
-                </>
-            </div>
-        </div>
-    );
+  // Create new plugin instance
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  
+  // for onchange event
+  const [pdfFile, setPdfFile]=useState(null);
+  const [pdfFileError, setPdfFileError]=useState('');
+
+  // for submit event
+  const [viewPdf, setViewPdf]=useState(null);
+
+  // onchange event
+  const fileType=['application/pdf'];
+  const handlePdfFileChange=(e)=>{
+    let selectedFile=e.target.files[0];
+    if(selectedFile){
+      if(selectedFile&&fileType.includes(selectedFile.type)){
+        let reader = new FileReader();
+            reader.readAsDataURL(selectedFile);
+            reader.onloadend = (e) =>{
+              setPdfFile(e.target.result);
+              setPdfFileError('');
+            }
+      }
+      else{
+        setPdfFile(null);
+        setPdfFileError('Please select valid pdf file');
+      }
+    }
+    else{
+      console.log('select your file');
+    }
+  }
+
+  // form submit
+  const handlePdfFileSubmit=(e)=>{
+    e.preventDefault();
+    if(pdfFile!==null){
+      setViewPdf(pdfFile);
+    }
+    else{
+      setViewPdf(null);
+    }
+  }
+
+  return (
+    <div className='container'>
+
+    <br></br>
+    
+      <form className='form-group' onSubmit={handlePdfFileSubmit}>
+        <input type="file" className='form-control'
+          required onChange={handlePdfFileChange}
+        />
+        {pdfFileError&&<div className='error-msg'>{pdfFileError}</div>}
+        <br></br>
+        <button type="submit" className='btn btn-success btn-lg'>
+          UPLOAD
+        </button>
+      </form>
+      <br></br>
+      <h4>View PDF</h4>
+      <div className='pdf-container'>
+        {/* show pdf conditionally (if we have one)  */}
+        {viewPdf&&<><Worker workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
+          <Viewer fileUrl={viewPdf}
+            plugins={[defaultLayoutPluginInstance]} />
+      </Worker></>}
+
+      {/* if we dont have pdf or viewPdf state is null */}
+      {!viewPdf&&<>No pdf file selected</>}
+      </div>
+
+    </div>
+  )
 }
-export default FileUpload
+
+export default App
